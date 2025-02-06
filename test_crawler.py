c16a5d21d9ebb078c0d36c4de0f4fce05d6c0988
RoCry <crysheen@gmail.com>
2025-02-06 20:19:49 +0800

    refactor: 🧹 Remove unused crawler module and tests

--- test_crawler.py
+++
@@ -1,8 +0,0 @@
-from crawler import url_to_markdown
-
-
-def test_random_webpage():
-    url = "https://www.latent.space/p/2025-papers"
-    result = url_to_markdown(url)
-    assert result is not None
-    print(result)
